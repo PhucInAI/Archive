ae897031f803ee4b47abf7e29e28c17080cd9db5
Phuc Thanh Nguyen <PhucInAI>
2023-04-18 08:58:47 +0700

    update logic barrier

--- test/test_barrier_from_images.py
+++ test/test_barrier_from_images.py
@@ -69,20 +69,27 @@ def main():
     for img_path in tqdm(img_path_lst):
         img             =   cv2.imread(img_path)
         output          =   check_barrier.predict_barrier(img)
-        for box in output:
-            left, top, right, bottom    =   box
-            cv2.rectangle(img, (left,top), (right, bottom),color=[255,0,0], thickness=5)
+        for line in output:
+            print(line)
+            point_1, point_2        =   line
+            point_1                 =   [int(coor) for coor in point_1]
+            point_2                 =   [int(coor) for coor in point_2]
+            point_1[1]              =   point_1[1]-100
+            point_2[1]              =   point_2[1]+100
+            # cv2.line(img, point_1, point_2, color=[255,0,0], thickness=5)
+            cv2.rectangle(img, point_1, point_2,color=[0,0,255], thickness=5)
+        #     cv2.rectangle(img, (left,top), (right, bottom),color=[255,0,0], thickness=5)
 
-        barrier                     =   check_barrier.barriers[0]
-        left_top                    =   barrier[0]
-        left_bottom                 =   barrier[1]
-        right_top                   =   barrier[2]
-        right_bottom                =   barrier[3]
+        # barrier                     =   check_barrier.barriers[0]
+        # left_top                    =   barrier[0]
+        # left_bottom                 =   barrier[1]
+        # right_top                   =   barrier[2]
+        # right_bottom                =   barrier[3]
 
-        point_floor_1               =   [int((left_top[0]+left_bottom[0])/2), int((left_top[1]+left_bottom[1])/2)]
-        point_floor_2               =   [int((right_top[0]+right_bottom[0])/2), int((right_top[1]+right_bottom[1])/2)]
+        # point_floor_1               =   [int((left_top[0]+left_bottom[0])/2), int((left_top[1]+left_bottom[1])/2)]
+        # point_floor_2               =   [int((right_top[0]+right_bottom[0])/2), int((right_top[1]+right_bottom[1])/2)]
 
-        cv2.line(img, point_floor_1, point_floor_2, color=[255,0,0], thickness=5)
+        # cv2.line(img, point_floor_1, point_floor_2, color=[255,0,0], thickness=5)
 
         cv2.imwrite(os.path.join(OUTPUT_DIR, os.path.basename(img_path)), img)
     
